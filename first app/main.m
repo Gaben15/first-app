f5782f2c7e98a0b0a6a9bb538dba730ad3e3a6fa
Gabe <Gabe@Gabes-MacBook-Pro.local>
2013-05-18 21:38:58 -0400

    Initial Commit

---
+++ first app/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  first app
+//
+//  Created by Gabe on 5/18/13.
+//  Copyright (c) 2013 Gabe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
